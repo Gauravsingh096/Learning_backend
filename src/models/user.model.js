4d13edab022bdf5b99068f84f377a89d0bff1196
Gauravsingh096 <singhgaurav07122004@gmail.com>
2024-05-12 02:17:14 +0530

    models_user

---
+++ src/models/user.model.js
@@ -0,0 +1,52 @@
+import mongoose, {Schema} from "mongoose";
+
+const userSchema =  new Schema(
+    {
+        username :{
+            type:String,
+            required: true,
+            unique: true,
+            lower: true,
+            trim: true,
+            index : true
+        },
+        email :{
+            type:String,
+            required: true,
+            unique: true,
+            lower: true,
+            trim: true,
+        },
+        fullname:{
+            type:String,
+            required: true,
+            trim: true,
+            index: true
+        },
+        avatar:{
+            type : String, // cloudenary
+            required : true,
+        },
+        coverImage:{
+            type : String, 
+        
+        },
+        watchHistory:[
+            {
+                type:Schema.Types.ObjectId,
+                ref:"Video"
+            }
+        ],
+        password :{
+            type:String,
+            required:[true, 'Password is required'],
+
+        },
+        refreshToken:{
+            type:String,
+
+        }   
+
+},{timestamps:true})
+
+export const User = mongoose.model("User", userSchema)
