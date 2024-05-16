73618bbf9e45ab5cab70ccb820e45691aae9392e
Gauravsingh096 <singhgaurav07122004@gmail.com>
2024-05-16 18:43:09 +0530

    add register controller

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -13,7 +13,7 @@ const uploadOnCloudinary = async (localFilePath) =>{
         const response = await cloudinary.uploader.upload(localFilePath,{
             resource_type:"auto"
         })
-        console.log("file is uploaded!!! on cloud!!",response.url);
+        console.log("file is uploaded!!! on cloudinary!!",response.url);
         return response;
 
     } catch(error){
