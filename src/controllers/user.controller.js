d4ab97bc58b92dda5a9b72543b479dcb03be7893
Gauravsingh096 <singhgaurav07122004@gmail.com>
2024-05-17 19:16:42 +0530

    user_error_remove

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -24,8 +24,15 @@ const registerUser = asyncHandler(async (req,res) =>{
     if(existedUser){
         throw new ApiError(409,"User with email or username exist")
     }
+    // console.log(req.files);
+
     const avatarLocalPath = req.files?.avatar?.[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0){
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
     
     if(!avatarLocalPath){
         throw new ApiError(400,"Avatar missed")
