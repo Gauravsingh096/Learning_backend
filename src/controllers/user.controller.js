32f633c62b20955e1b53ca1eb3d2605cee4e257c
Gauravsingh096 <singhgaurav07122004@gmail.com>
2024-05-17 15:28:46 +0530

    "Fixed optional chaining in avatar and coverImage path assignments"

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -24,8 +24,8 @@ const registerUser = asyncHandler(async (req,res) =>{
     if(existedUser){
         throw new ApiError(409,"User with email or username exist")
     }
-    const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    const avatarLocalPath = req.files?.avatar?.[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
     
     if(!avatarLocalPath){
         throw new ApiError(400,"Avatar missed")
