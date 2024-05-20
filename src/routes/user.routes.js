a47e62700fb64891f205f76ea9f348d4c391afb5
Gauravsingh096 <singhgaurav07122004@gmail.com>
2024-05-20 23:29:25 +0530

    UPDATE TOKENS

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
 const router = Router()
 
@@ -11,6 +11,10 @@ router.route("/register").post(
     registerUser
 )
 // router.route("/login").post(login)
+
+router.route("/login").post(loginUser)
+// secure routes
+router.route("/logout").post(verifyJWT, logoutUser)
 
 
 
